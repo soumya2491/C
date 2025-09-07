6862a93b08d8e0f65cb67ec176eed3614114357b
soumya2491 <soumyachopra901@gmail.com>
2025-09-07 23:22:45 +0530

    learning about float data types

---
+++ classwork5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int main()
+ {
+ float a = 9.0f;
+ float b = 2.5f;
+//  2x10^-4
+ float c = 2E-4f;
+ printf("%f\n", a);
+ printf("%f\n", b);
+ printf("%f", c);
+ return 0;
+ }
+ //By default the floating point values are 6 digits
+ 
