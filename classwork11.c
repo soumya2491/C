5d5fc0e030cf6ae3ded36016b38c0dd4643e0f38
soumya2491 <soumyachopra901@gmail.com>
2025-09-07 23:32:43 +0530

    taking inputs

---
+++ classwork11.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+ // Driver code
+ int main() 
+{
+ int a, b;
+ printf("Enter first number: ");
+ scanf("%d", &a);
+ printf("Enter second number: ");
+ scanf("%d", &b);
+ printf("A : %d \t B : %d" , a , b);
+ return 0;
+ }
