176bf86110e7d4d341a000395681a16bc4c57e56
soumya2491 <soumyachopra901@gmail.com>
2025-09-07 23:33:10 +0530

    printing string

---
+++ classwork12.c
@@ -0,0 +1,14 @@
+//  #include <stdio.h>
+//  int main()
+//  {
+//  char a[] = "Hi ";
+//  printf("%s\n", a);
+//  return 0;
+//  }
+ #include <stdio.h>
+ int main()
+ {
+ char a[] = "Hi ";
+ printf("%s\n", a);
+ return 0;
+ }
