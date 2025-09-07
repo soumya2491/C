16b4abc618c2952efe86388f01916f797e300182
soumya2491 <soumyachopra901@gmail.com>
2025-09-07 23:25:07 +0530

    boolean data type

---
+++ classwork7.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>  // For printf()
+ #include <stdbool.h>  /* For boolean data type (bool, 
+ true, false)*/
+ // Main Function
+ int main() {
+ /*Boolean data types declared*/ 
+ bool a = true;  // 'a' initialized to true
+ bool b = false; // 'b' initialized to false
+ /* Printing Boolean values as integers (true = 1, false 
+ = 0)*/
+ printf("True : %d\n", a); 
+printf("False : %d\n", b); 
+return 0;
+ }
