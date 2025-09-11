66a68cdc7dce60af0ea5339b16c29d345efe622c
soumya2491 <soumyachopra901@gmail.com>
2025-09-11 23:26:41 +0530

    understanding for loop concepts

---
+++ classwork29.c
@@ -0,0 +1,34 @@
+// #include <stdio.h>
+
+// int main(){
+//      int a =1;
+//      for (; a<=5;){
+//         printf("a: %d\n",a++);
+//      }
+//      return 0;
+// }
+// #include <stdio.h>
+
+// int main(){
+//      int a =1;
+//      for (; a<=5; ) {
+        
+//         printf("a: %d\n",a++);
+//      }
+//      return 0;
+// }
+#include <stdio.h>
+
+int main(){
+     int a =1;
+     for (; ; ) {
+        
+        printf("a: %d\n",a++);
+        if (a==6)
+        break;
+     }
+     return 0;
+}
+
+
+
