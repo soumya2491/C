6cad31c7f0aaeec269d8ae2466cb3b5ad51e975a
soumya2491 <soumyachopra901@gmail.com>
2025-09-11 23:41:42 +0530

    using time library

---
+++ classwork47.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+int main(){
+     time_t curtime;
+     
+     time(&curtime);
+    printf("%s",ctime(&curtime));
+     return 0;
+}
+#include <stdio.h>
+
+int main(){
+     int n;
+        
+     return 0;
+}
