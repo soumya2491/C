7d802d69fd8a8f0dc6f83716c66b04849397ef4b
soumya2491 <soumyachopra901@gmail.com>
2025-09-11 23:40:58 +0530

    learning some std lib functions

---
+++ classwork46.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>  
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int abs_value = abs(num);
+
+    printf("The absolute value of %d is %d", num, abs_value);
+
+    return 0;
+}
+#include <stdio.h>
+
+int main(){
+    
+    return 0;
+}
