fc27986412172ce0c654b5515ebc32f75c3ce2c7
soumya2491 <soumyachopra901@gmail.com>
2025-09-11 23:34:34 +0530

    practicing problem statements

---
+++ classwork41.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void employee_details();
+
+int main(){
+employee_details();
+     return 0;
+}
+void employee_details(){
+    int a,b;
+    char name[100];
+    char designation [100];
+    scanf("%d %d %s %s",&a,name,designation);
+   printf("%d\n %d\n %s\n %s\n",a,b,name,designation);
+  
+}
+int tax_deduction(){
+    int b;
+    if (b>10000)
+}
