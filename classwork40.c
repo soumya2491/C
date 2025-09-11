3f7b4aeee60eab486dd44fffe1104e11bedb9061
soumya2491 <soumyachopra901@gmail.com>
2025-09-11 23:33:46 +0530

    learning basic libraries

---
+++ classwork40.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h> 
+
+int main() {
+    int x;
+    double result;
+    scanf("%d", &x);
+    result = pow(x,0.5 );
+    printf("%.4f\n", result); 
+    return 0;
+}
