beafcf8c40c65287464ef0ec4575776546a9f3ec
soumya2491 <soumyachopra901@gmail.com>
2025-09-11 23:35:54 +0530

    learning some functions of math libraries

---
+++ classwork42.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float number;
+    printf("Enter a number: ");
+    scanf("%f", &number);
+    printf("Ceil of %.2f is %.2f\n", number, ceil(number));
+    printf("Floor of %.2f is %.2f\n", number, floor(number));
+
+    return 0;
+}
+#include <stdio.h>
+
+int main(){
+    int n;
+    return 0;
+}
