beabe90ce3850be32034279e0e3393cdb1ce1e16
soumya2491 <soumyachopra901@gmail.com>
2025-09-13 00:01:15 +0530

    c types

---
+++ classwork55.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+
+// int main(){
+//     auto int x = 95;
+//     printf("%d", x);
+//     return 0;
+// }
+#include <stdio.h>
+
+int main(){
+    int a;
+    char b;
+    float c;
+    
+    printf("%d" "%c" "%d", a,b,c);
+    return 0;
+}
