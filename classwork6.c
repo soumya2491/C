d72c931558c34be36f47f730db30e56eef1fcb2c
soumya2491 <soumyachopra901@gmail.com>
2025-09-16 00:00:05 +0530

    add C program demonstrating string literal concatenation and alert escape sequence

---
+++ classwork6.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+ int main(void)
+ {
+ // output may depend upon the compiler
+ printf("My mobile number "
+ "is 7\a8\a7\a3\a9\a2\a3\a4\a0\a8\a");
+ return (0);
+ }
+ #include <stdio.h>
+ 
+ int main(){
+    
+    return 0;
+ }
